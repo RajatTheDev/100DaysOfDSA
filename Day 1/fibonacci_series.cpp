f5bf628b8dd4a3155f8dc192204b004ac52efb52
RajatTheDev <RajatTheDev@users.noreply.github.com>
2024-07-25 23:00:33 +0530

    Day 1

---
+++ Day 1/fibonacci_series.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    int n = 10, a = 0, b = 1;
+    cout << a << " " << b << " ";
+    for (int i = 1; i <= n; i++)
+    {
+        int nextNum = a + b;
+        cout << nextNum << " ";
+        a = b;
+        b = nextNum;
+    }
+    cout << endl;
+    return 0;
+}
